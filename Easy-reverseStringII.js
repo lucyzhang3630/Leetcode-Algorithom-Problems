f1538f9db4542495ce9709965bea0ea0e1d6c573
Lucy Zhang <lucyzhang3630@yahoo.com>
2019-01-08 17:37:26 -0600

    add one solution to problem reverse String II

--- Easy-reverseStringII.js
+++ Easy-reverseStringII.js
@@ -11,6 +11,7 @@
  * @param {string} s
  * @param {number} k
  * @return {string}
+ * solution 1
  */
 var reverse = (arr, fr, to) => {
     for (let i = fr, j = to; i < j; i++, j--) {
@@ -27,4 +28,26 @@ var reverseStr = (s, k) => {
         reverse(chars, i, j);
     }
     return chars.join('');
+};
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ * solution 2
+ */
+var reverseStr = function(s, k) {
+    var strArr = s.split("");
+    for(var i = 0; i<strArr.length;i+=2*k){
+        //when it comes to the last slice, comparing i+k-1 with strArr.length-1, to see if the remaining is less than k
+        //to reverse an array or string, it's better to do it with i=0,j=length-1,i++,j--,
+        //if i<j, swap the value for element at position i and j
+        var j = Math.min(i+k-1,strArr.length-1);
+        for(var m = i, l = j;m<l;m++,l--) {
+            var temp = strArr[m];
+            strArr[m] = strArr[l];
+            strArr[l] = temp;
+        }
+    }
+    return strArr.join("")
 };
