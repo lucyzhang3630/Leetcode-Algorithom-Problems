6023d3a3ea73c09f7246ffaa8acb7950d9582efa
Lucy Zhang <lucyzhang3630@yahoo.com>
2019-02-04 23:07:01 -0600

    update the solution, still need to be improved

--- Easy-stringCompression.js
+++ Easy-stringCompression.js
@@ -4,13 +4,27 @@
  */
 var compress = function(chars) {
     var counter;
-    for(var i = 0;i<chars.length;){
-        counter = 1;
-        for(var j=i+1;j<chars.length;j++){
-            if(chars[j]===chars[i]){
-                counter++
-            }
+    var index = 0;
+    var i = 0;
+    var length = chars.length;
+    var char = "";
+    while(i<length){
+        console.log(i);
+        counter = 0;
+        char = chars[i];
+        while(i<length&&chars[i] === char){
+            counter++;
+            i++;
         }
+        if(counter>1){
+            chars.splice(index+1,counter-1,...counter.toString().split(""));
+            index += counter.toString().split("").length + 1;
+        }else{
+            index +=1
+        }
+        //console.log(i);
+        // console.log(index);
+        // console.log(chars);
     }
 
     return chars.length
