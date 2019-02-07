1a477029f7e2c976cbefe34242a7620c4fad33fd
Lucy Zhang <lucyzhang3630@yahoo.com>
2019-02-06 23:28:48 -0600

    a possible solution using 2 pointers

--- Easy-stringCompression.js
+++ Easy-stringCompression.js
@@ -22,9 +22,29 @@ var compress = function(chars) {
         }else{
             index +=1
         }
-        
+
     }
 
     return chars.length
 
+};
+//solution : 2 pointer
+var compress = function(chars) {
+    let i = 0, j = 0;
+    while (i < chars.length) {
+        if (chars[j] === chars[i]) {
+            j++;
+        } else if (j > i + 1) {
+          // if more than one char in the array
+            const nums = (j - i).toString().split('');
+            chars.splice(i + 1, j - i - 1, ...nums);
+            j = i = i + 1 + nums.length;
+
+        } else if (j = i + 1) {
+            //if, only one char in the array
+            i = j;
+            j++;
+        }
+    }
+    return chars.length;
 };
