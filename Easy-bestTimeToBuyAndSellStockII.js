c150d37c2b8ebc00d7b3f49516cebd77c4bba880
Lucy Zhang <lucyzhang3630@yahoo.com>
2019-02-17 23:52:00 -0600

    add todo

--- Easy-bestTimeToBuyAndSellStockII.js
+++ Easy-bestTimeToBuyAndSellStockII.js
@@ -3,7 +3,7 @@
 // Design an algorithm to find the maximum profit. You may complete as many transactions
 // as you like (i.e., buy one and sell one share of the stock multiple times).
 //
-// Note: You may not engage in multiple transactions at the same time 
+// Note: You may not engage in multiple transactions at the same time
 // (i.e., you must sell the stock before you buy again).
 //
 // Example 1:
@@ -36,3 +36,4 @@ var maxProfit = function(prices) {
     }
     return max
 };
+// TODO: solution 2 
